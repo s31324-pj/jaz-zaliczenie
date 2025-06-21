9b73d78eaa1466aa0c4f602e255670a639223776
Patryk Latkowski <s31324@pjwstk.edu.pl>
2025-06-21 10:20:35 +0200

    Initialized program

---
+++ src/main/java/pl/pjatk/jaz_s31324_nbp/JazS31324NbpApplication.java
@@ -0,0 +1,13 @@
+package pl.pjatk.jaz_s31324_nbp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JazS31324NbpApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JazS31324NbpApplication.class, args);
+	}
+
+}
---
+++ src/test/java/pl/pjatk/jaz_s31324_nbp/JazS31324NbpApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.pjatk.jaz_s31324_nbp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JazS31324NbpApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
